c26f672a05099047ae0fc8b94adcade7be2c3be8
Apple <apple@Apples-MacBook-Air.local>
2019-06-16 01:22:31 +0530

    Task automation and transpiling

---
+++ buildScript/srcServer.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+
+app.listen(port, function(err){
+
+  if(err){
+    console.log(err);
+  } else {
+    open('http://localhost:'+ port);
+  }
+
+})
